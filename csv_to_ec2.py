8b9492548a24155f721e2c1689707e6bf692f98c
박승균 <tmdrbs6755@gmail.com>
2025-10-17 20:48:00 +0900

    csv_to_ec2 upload

---
+++ csv_to_ec2.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# CSV 파일 불러오기
+df = pd.read_csv("Data/musinsa_pick.csv")
+
+# MySQL 연결 설정
+user = 'redash_user'
+password = 'tmdrbs6755'
+host = '3.39.21.171'
+port = 3306
+database = 'musinsa_db'
+
+# SQLAlchemy 엔진 생성
+engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}")
+
+# 테이블로 적재 (기존 테이블에 추가 삽입)
+df.to_sql(name='musinsa_pick', con=engine, if_exists='append', index=False)
